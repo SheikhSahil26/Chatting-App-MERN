a58132b7f23b0952a14bf8ac00d4be5e7e8ee252
SheikhSahil26 <sahilsheikh261204@gmail.com>
2025-01-18 23:32:24 +0530

    allow credentials

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.options("*", (req, res) => {
     res.header("Access-Control-Allow-Origin", "https://baatchit-lyart.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.sendStatus(200);
 });
 

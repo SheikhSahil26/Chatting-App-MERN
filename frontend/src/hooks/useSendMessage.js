8f2ffca06c3146cdc47734520472996bdff4b5c5
SheikhSahil26 <sahilsheikh261204@gmail.com>
2025-01-19 18:29:28 +0530

    added backend url again absolute

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -8,7 +8,7 @@ const useSendMessage = () => {
 
     const sendMessage = async (message)=>{
         try{
-            const res=await fetch(`/api/message/send/${selectedConversation._id}`,
+            const res=await fetch(`https://chatapp-sigma-eight.vercel.app/api/message/send/${selectedConversation._id}`,
                 {
                 method:'POST',
                 headers:{
